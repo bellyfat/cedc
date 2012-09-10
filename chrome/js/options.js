ecd98671cc45cf581d84ad32562e717b93b34098
U-SYNBUF\krufe <krufe@KRUFE-WIN7.synbuf.local>
2012-09-10 00:38:57 -0400

    Options page functionality complete

--- chrome/js/options.js
+++ chrome/js/options.js
@@ -1,51 +1,159 @@
-// Saves options to localStorage.
-function save_options() {
-  var select = document.getElementById("color");
-  var color = select.children[select.selectedIndex].value;
-  localStorage["favorite_color"] =  color;
-
-  // Update status to let user know options were saved.
-  var status = document.getElementById("status");
-  status.innerHTML = "Options Saved.";
-  setTimeout(function() {
-    status.innerHTML = "";
-  }, 750);
-}
-
-// Restores select box state to saved value from localStorage.
-function restore_options() {
-/*	var favorite = localStorage["favorite_color"];
-	
-	if (!favorite)
-	{
-		return;
+Object.size = function(obj) {
+	var size = 0, key;
+	for (key in obj) {
+	if (obj.hasOwnProperty(key)) size++;
 	}
-	
-	var select = document.getElementById("color");
-	
-	for (var i = 0; i < select.children.length; i++)
-	{
-		var child = select.children[i];
-
-		if (child.value == favorite)
-		{
-			child.selected = "true";
-
-			break;
-		}
-	}
-*/
-}
+	return size;
+};
 
 //Generate a random key, put it in the input
 function generate_key()
 {
-	$("#key").val(CryptoJS.AES.encrypt("test", "hi"));
+	$("#key").val(CryptoJS.lib.WordArray.random(256).toString(CryptoJS.enc.Base64)
+		+ "::" + CryptoJS.lib.WordArray.random(32).toString(CryptoJS.enc.Base64));
+}
+
+function select_key()
+{
+	$("#key").focus().select();
+}
+
+function save_key_encrypt()
+{
+	var k = $("#key_to_save_encrypt").val();
+	var i = $("#ident_to_save_encrypt").val();
+	var arr = {};
+
+	if(k == "" || i == "")
+	{
+		return;
+	}
+
+	if(localStorage["saved_encrypt_keys"] != null)
+	{
+		arr = JSON.parse(localStorage["saved_encrypt_keys"]);
+	}
+
+	arr[i] = k;
+	localStorage["saved_encrypt_keys"] = JSON.stringify(arr);
+
+	populateSavedKeyEncryptDiv();
+}
+
+function save_key_decrypt()
+{
+	var k = $("#key_to_save_decrypt").val();
+	var i = $("#ident_to_save_decrypt").val();
+	var arr = {};
+
+	if(k == "" || i == "")
+	{
+		return;
+	}
+
+	if(localStorage["saved_decrypt_keys"] != null)
+	{
+		arr = JSON.parse(localStorage["saved_decrypt_keys"]);
+	}
+
+	arr[i] = k;
+	localStorage["saved_decrypt_keys"] = JSON.stringify(arr);
+
+	populateSavedKeyDecryptDiv();
+}
+
+function populateSavedKeyEncryptDiv()
+{
+	if(localStorage["saved_encrypt_keys"] == null)
+	{
+		return;
+	}
+
+	var arr = JSON.parse(localStorage["saved_encrypt_keys"]);
+
+	if(Object.size(arr) == 0)
+	{
+		$("#saved_keys_encrypt").html("");
+
+		return;
+	}
+
+	var output = "<table border='1'><th>X</th><th>Label</th><th>Encrypt Key</th>";
+
+	for(var key in arr)
+	{
+		output = output + "<tr><td><input type='checkbox' class='delete_encrypt' value='" + key + "'></input></td><td>" + key + "</td><td><textarea rows='5' cols='100' readonly='readonly'>" + arr[key] + "</textarea></td></tr>";
+	}
+
+	output = output + "</table><br />";
+
+	$("#saved_keys_encrypt").html(output);
+	$("#ident_to_save_encrypt").val("");
+	$("#key_to_save_encrypt").val("");
+}
+
+function populateSavedKeyDecryptDiv()
+{
+	if(localStorage["saved_decrypt_keys"] == null)
+	{
+		return;
+	}
+
+	var arr = JSON.parse(localStorage["saved_decrypt_keys"]);
+
+	if(Object.size(arr) == 0)
+	{
+		$("#saved_keys_decrypt").html("");
+
+		return;
+	}
+
+	var output = "<table border='1'><th>X</th><th>Label</th><th>Decrypt Key</th>";
+
+	for(var key in arr)
+	{
+		output = output + "<tr><td><input type='checkbox' class='delete_decrypt' value='" + key + "'></input></td><td>" + key + "</td><td><textarea rows='5' cols='100' readonly='readonly'>" + arr[key] + "</textarea></td></tr>";
+	}
+
+	output = output + "</table><br />";
+
+	$("#saved_keys_decrypt").html(output);
+	$("#ident_to_save_decrypt").html("");
+	$("#key_to_save_decrypt").html("");
+}
+
+function delete_key_encrypt()
+{
+	$("div#saved_keys_encrypt :checked").each(function() {
+		var arr = JSON.parse(localStorage["saved_encrypt_keys"]);
+		delete arr[$(this).val()];
+		localStorage["saved_encrypt_keys"] = JSON.stringify(arr);
+	});
+
+	populateSavedKeyEncryptDiv();
+}
+
+function delete_key_decrypt()
+{
+	$("div#saved_keys_decrypt :checked").each(function() {
+		var arr = JSON.parse(localStorage["saved_decrypt_keys"]);
+		delete arr[$(this).val()];
+		localStorage["saved_decrypt_keys"] = JSON.stringify(arr);
+	});
+
+	populateSavedKeyDecryptDiv();
 }
 
 
-//Add listeners
 
+//Add listeners
 document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('button#genkey').addEventListener('click', generate_key);
+	populateSavedKeyEncryptDiv();
+	populateSavedKeyDecryptDiv();
+	document.querySelector('button#genkey').addEventListener('click', generate_key);
+	document.querySelector('textarea#key').addEventListener('click', select_key);
+	document.querySelector('button#save_key_encrypt').addEventListener('click', save_key_encrypt);
+	document.querySelector('button#save_key_decrypt').addEventListener('click', save_key_decrypt);
+	document.querySelector('button#delete_key_encrypt').addEventListener('click', delete_key_encrypt);
+	document.querySelector('button#delete_key_decrypt').addEventListener('click', delete_key_decrypt);
 });
